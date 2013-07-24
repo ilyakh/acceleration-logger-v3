cae4845c9ee94c0c2ab57c8bf348121aab629a68
ilyakh <ilyakh@ulrik.uio.no>
2013-07-24 19:50:54 +0200

    stable 100Hz

--- logger_v3.ino
+++ logger_v3.ino
@@ -5,139 +5,43 @@
 
 MPU6050 mpu(0x69);
 
-
-
-#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
-bool blinkState = false;
-
-// MPU control/status vars
-bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
-
-// orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 acceleration;
 VectorInt16 rotation;
-// VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-// VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-// VectorFloat gravity;    // [x, y, z]            gravity vector
-// float euler[3];         // [psi, theta, phi]    Euler angle container
-// float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
 
-int FREQUENCY_DELAY = 5;
+int FREQUENCY_DELAY = 10;
+int LOGGER_BAUDRATE = 115200;
 
 
-
-
-// ================================================================
-// ===               INTERRUPT DETECTION ROUTINE                ===
-// ================================================================
-
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
-    mpuInterrupt = true;
-}
-
-
-
-// ================================================================
-// ===                      INITIAL SETUP                       ===
-// ================================================================
 
 void setup() {
-    // join I2C bus (I2Cdev library doesn't do this automatically)
+    
     Wire.begin();
     TWBR = 12; // change I2C to 400 mHz
     
-    Serial2.begin( 921600 );
-    while (!Serial2); // wait for Leonardo enumeration, others continue immediately
-    
-    // initialize device
-    Serial2.println(F("Initializing I2C devices..."));
+    Serial2.begin( 115200 );
     mpu.initialize();
 
-
-
-    // verify connection
-    Serial2.println(F("Testing device connections..."));
-    Serial2.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-    // load and configure the DMP
-    Serial2.println(F("Initializing DMP..."));
-    devStatus = mpu.dmpInitialize();
-    
-    
-    
-    // ================================================================
-    // ===                      SETTINGS                            ===
-    // ================================================================
         
     mpu.setRate( FREQUENCY_DELAY );
     mpu.setFullScaleGyroRange( MPU6050_GYRO_FS_500 );
     mpu.setFullScaleAccelRange( MPU6050_ACCEL_FS_8 );
     
-    // debug info
-    Serial2.print( "Accelerometer sample rate: " );
-    Serial2.println( mpu.getRate() );
-    
-    
-    // make sure it worked (returns 0 if so)
-    if (devStatus == 0) {
-        // turn on the DMP, now that it's ready
-        Serial2.println(F("Enabling DMP..."));
-        mpu.setDMPEnabled( true );
-
-        // enable Arduino interrupt detection
-        Serial2.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
-        attachInterrupt(0, dmpDataReady, RISING);
-        mpuIntStatus = mpu.getIntStatus();
-
-        // set our DMP Ready flag so the main loop() function knows it's okay to use it
-        Serial2.println(F("DMP ready! Waiting for first interrupt..."));
-        dmpReady = true;
-
-        // get expected DMP packet size for later comparison
-        packetSize = mpu.dmpGetFIFOPacketSize();
-        
-    } else {
-        // ERROR!
-        // 1 = initial memory load failed
-        // 2 = DMP configuration updates failed
-        // (if it's going to break, usually the code will be 1)
-        Serial2.print( F("DMP Initialization failed (code ") );
-        Serial2.print( devStatus );
-        Serial2.println( F(")") );           
-        
-    }
-    
-    delay( 1000 );
-    
     FlexiTimer2::set( FREQUENCY_DELAY, store );
     FlexiTimer2::start();
-
-    // configure LED for output
-    pinMode( LED_PIN, OUTPUT );
     
-
 }
-
-
 
 void store() {
   
   Serial2.print( millis() );
   Serial2.print( "," );
   
-  Serial2.print( aa.x );
+  Serial2.print( acceleration.x );
   Serial2.print( "," );
-  Serial2.print( aa.y );
+  Serial2.print( acceleration.y );
   Serial2.print( "," );
-  Serial2.print( aa.z );
+  Serial2.print( acceleration.z );
   Serial2.print( "," );
   
   Serial2.print( rotation.x );
@@ -154,7 +58,7 @@ void store() {
 
 void loop() {
   
-    mpu.getAcceleration( &aa.x, &aa.y, &aa.z );
+    mpu.getAcceleration( &acceleration.x, &acceleration.y, &acceleration.z );
     mpu.getRotation( &rotation.x, &rotation.y, &rotation.z );
     
 }
