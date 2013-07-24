eea6176a5718c1319914985c04010b62102b737e
ilyakh <ilyakh@ulrik.uio.no>
2013-07-24 17:15:38 +0200

    corrected sample rate

--- logger_v3.ino
+++ logger_v3.ino
@@ -1,6 +1,7 @@
 #include "Wire.h"
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
+#include "FlexiTimer2.h"
 
 MPU6050 mpu(0x69);
 
@@ -51,7 +52,7 @@ void setup() {
     // join I2C bus (I2Cdev library doesn't do this automatically)
     Wire.begin();
     
-    Serial2.begin(115200);
+    Serial2.begin( 115200 );
     while (!Serial2); // wait for Leonardo enumeration, others continue immediately
     
     // initialize device
@@ -77,7 +78,17 @@ void setup() {
     devStatus = mpu.dmpInitialize();
     
     
-    mpu.setRate( 200 );
+    
+    // ================================================================
+    // ===                      SETTINGS                            ===
+    // ================================================================
+        
+    mpu.setRate( 5 );
+    
+    mpu.setFullScaleGyroRange( MPU6050_GYRO_FS_250 );
+    mpu.setFullScaleAccelRange( MPU6050_ACCEL_FS_8 );
+    
+    // debug info
     Serial2.print( "Accelerometer sample rate: " );
     Serial2.println( mpu.getRate() );
     
@@ -86,7 +97,7 @@ void setup() {
     if (devStatus == 0) {
         // turn on the DMP, now that it's ready
         Serial2.println(F("Enabling DMP..."));
-        mpu.setDMPEnabled(true);
+        mpu.setDMPEnabled( true );
 
         // enable Arduino interrupt detection
         Serial2.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
@@ -140,6 +151,37 @@ void loop() {
         // .
         // .
         // .
+        
+        // read and save dmp sensor values
+        mpu.dmpGetAccel( &aa, fifoBuffer );
+        mpu.dmpGetQuaternion( &q, fifoBuffer );
+        mpu.dmpGetYawPitchRoll( ypr, &q, &gravity );
+        
+        // read and save the raw sensor values
+        // mpu.getAcceleration( &aa.x, &aa.y, &aa.z );
+        // mpu.getRotation( &rotation.x, &rotation.y, &rotation.z );        
+        
+        
+        Serial2.print( millis() );
+        Serial2.print( "," );
+        Serial2.print( aa.x );
+        Serial2.print( "," );
+        Serial2.print( aa.y );
+        Serial2.print( "," );
+        Serial2.print( aa.z );
+        
+        Serial2.print( "," );
+        Serial2.print( ypr[0] );
+        Serial2.print( "," );
+        Serial2.print( ypr[1] );
+        Serial2.print( "," );
+        Serial2.print( ypr[2] );
+        
+        
+        // end record
+        Serial2.println();
+        
+        
     }
 
     // reset interrupt flag and get INT_STATUS byte
@@ -168,33 +210,9 @@ void loop() {
         fifoCount -= packetSize;
         
         
-        // read and save dmp sensor values
-        mpu.dmpGetAccel( &aa, fifoBuffer );
-        mpu.dmpGetQuaternion( &q, fifoBuffer );
-        mpu.dmpGetYawPitchRoll( ypr, &q, &gravity );
+
         
-        // read and save the raw sensor values
-        // mpu.getAcceleration( &aa.x, &aa.y, &aa.z );
-        // mpu.getRotation( &rotation.x, &rotation.y, &rotation.z );
-        
-        Serial2.print( millis() );
-        Serial2.print( "," );
-        Serial2.print( aa.x );
-        Serial2.print( "," );
-        Serial2.print( aa.y );
-        Serial2.print( "," );
-        Serial2.print( aa.z );
-        
-        Serial2.print( "," );
-        Serial2.print( ypr[0] );
-        Serial2.print( "," );
-        Serial2.print( ypr[1] );
-        Serial2.print( "," );
-        Serial2.print( ypr[2] );
-        
-        
-        // end record
-        Serial2.println();
+
         
         
 
