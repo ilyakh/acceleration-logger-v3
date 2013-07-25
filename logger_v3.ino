e5c1acd7ee706d80b30e2f88d581a4dd0f7e2d38
ilyakh <ilyakh@ulrik.uio.no>
2013-07-25 02:16:50 +0200

    final commit

--- logger_v3.ino
+++ logger_v3.ino
@@ -2,23 +2,23 @@
 #include "Wire.h"
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
- 
+#include "EEPROM.h"
 
 MPU6050 mpu( 0x69 );
 
 VectorInt16 acceleration;
 VectorInt16 rotation;
 
-
+int axMax, axMin, ayMax, ayMin, azMax, azMin;
+int axOffset, ayOffset, azOffset;
 
 int SENSOR_DELAY = 5; // 200 Hz
 int WRITE_DELAY = 10; // 100 Hz
 
-int LOGGER_BAUDRATE = 345600;
+#define LOGGER_BAUDRATE 115200
 
 volatile int counter = 0;
 volatile long sum[6];
-
 
 enum Coordinates {
   ax, ay, az,
@@ -28,17 +28,24 @@ enum Coordinates {
 
 
 void setup() {
-    
     Wire.begin();
+    Serial.begin( LOGGER_BAUDRATE );
     
     mpu.initialize();
     
     mpu.setRate( SENSOR_DELAY );
     mpu.setFullScaleGyroRange( MPU6050_GYRO_FS_500 );
     mpu.setFullScaleAccelRange( MPU6050_ACCEL_FS_8 );
-
-    Serial2.begin( 921600 );
-        
+    
+    delay(100);
+    readEEPROM();
+    delay(100);
+    
+    axOffset = 0;
+    ayOffset = 0;
+    azOffset = 0;
+    
+    
     FlexiTimer2::set( WRITE_DELAY, store );
     FlexiTimer2::start();
         
@@ -52,9 +59,9 @@ void loop() {
     mpu.getRotation( &rotation.x, &rotation.y, &rotation.z );
     
     // update the sum of axes
-    sum[ax] += acceleration.x;
-    sum[ay] += acceleration.y;
-    sum[az] += acceleration.z;
+    sum[ax] += (acceleration.x-axOffset);
+    sum[ay] += (acceleration.y-ayOffset);
+    sum[az] += (acceleration.z-azOffset);
     
     sum[rx] += rotation.x;
     sum[ry] += rotation.y;
@@ -65,26 +72,26 @@ void loop() {
 }
 
 void store() {
- 
-    Serial2.print( millis() );
-    Serial2.print( "," );
-    Serial2.print( counter );
-    Serial2.print( "," );
+
+    Serial.print( millis() );
+    Serial.print( "," );
+    Serial.print( counter );
+    Serial.print( "," );
     
-    Serial2.print( sum[ax] / counter );
-    Serial2.print( "," );
-    Serial2.print( sum[ay] / counter );
-    Serial2.print( "," );
-    Serial2.print( sum[az] / counter );
-    Serial2.print( "," );
+    Serial.print( sum[ax] / counter );
+    Serial.print( "," );
+    Serial.print( sum[ay] / counter );
+    Serial.print( "," );
+    Serial.print( sum[az] / counter );
+    Serial.print( "," );
+        
+    Serial.print( sum[rx] / counter );
+    Serial.print( "," );
+    Serial.print( sum[ry] / counter );
+    Serial.print( "," );
+    Serial.print( sum[rz] / counter );
     
-    Serial2.print( sum[rx] / counter );
-    Serial2.print( "," );
-    Serial2.print( sum[ry] / counter );
-    Serial2.print( "," );
-    Serial2.print( sum[rz] / counter );
-    
-    Serial2.println();
+    Serial.println();
 
     
     // reset
@@ -94,3 +101,19 @@ void store() {
     counter = 0; 
     
 }
+
+void readEEPROM(){
+  
+  // Serial.println("\nReading EEPROM... ");
+  axMin = (EEPROM.read(0) * 256) + EEPROM.read(1) - 32768;
+  axMax = (EEPROM.read(2) * 256) + EEPROM.read(3) - 32768;
+  ayMin = (EEPROM.read(4) * 256) + EEPROM.read(5) - 32768;
+  ayMax = (EEPROM.read(6) * 256) + EEPROM.read(7) - 32768;
+  azMin = (EEPROM.read(8) * 256) + EEPROM.read(9) - 32768;
+  azMax = (EEPROM.read(10) * 256) + EEPROM.read(11) - 32768;
+  axOffset = (int)(axMax + axMin)/2;
+  ayOffset = (int)(ayMax + ayMin)/2;
+  azOffset = (int)(azMax + azMin)/2;
+  
+}
+
